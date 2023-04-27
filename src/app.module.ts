c87f4cc86b0b16b1abc53df791617198bc4b123c
muhammad kadi <muhammad.kadi.1999@gmail.com>
2023-04-27 11:39:56 +0300

    add traffic lights

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrafficLightsModule } from './traffic_lights/traffic_lights.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     UsersModule,
+    TrafficLightsModule,
     MongooseModule.forRoot(
       'mongodb+srv://sheryan:sheryan@sheryan.piccwzz.mongodb.net/?retryWrites=true&w=majority',
     ),
